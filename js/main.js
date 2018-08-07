4de1930e8a032e78e498045eb93b33512f143346
buyuanlee <buyuanlee@gmail.com>
2018-08-07 21:52:39 +0800

    第一版完成

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var bar = document.getElementById('portfolioBar');
+portfolioAll.onclick = function() {
+  bar.className = 'scrollBar state-1'
+}
+portfolioFrame.onclick = function() {
+  bar.className = 'scrollBar state-2'
+}
+portfolioNative.onclick = function() {
+  bar.className = 'scrollBar state-3'
+}
