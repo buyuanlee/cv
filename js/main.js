0d473e4602f7c52c7009ae9e27c187d7f79ca728
buyuanlee <buyuanlee@gmail.com>
2018-08-21 17:26:42 +0800

    增加导航条二级菜单及样式

--- js/main.js
+++ js/main.js
@@ -1,17 +1,27 @@
 var bar = document.getElementById('portfolioBar');
-portfolioAll.onclick = function() {
-  bar.className = 'scrollBar state-1'
+portfolioAll.onclick = function () {
+    bar.className = 'scrollBar state-1'
 }
-portfolioFrame.onclick = function() {
-  bar.className = 'scrollBar state-2'
+portfolioFrame.onclick = function () {
+    bar.className = 'scrollBar state-2'
 }
-portfolioNative.onclick = function() {
-  bar.className = 'scrollBar state-3'
+portfolioNative.onclick = function () {
+    bar.className = 'scrollBar state-3'
 }
-window.onscroll = function(navScroll){
-if (window.scrollY>0){
-    topNavBar.classList.add('sticky')
-} else{
-    topNavBar.classList.remove('sticky')
+window.onscroll = function (navScroll) {
+    if (window.scrollY > 0) {
+        topNavBar.classList.add('sticky')
+    } else {
+        topNavBar.classList.remove('sticky')
+    }
 }
+let liTags = document.querySelectorAll('nav.meanu>ul>li')
+for (let i = 0; i < liTags.length; i++) {
+    liTags[i].onmouseenter = function (x) {
+        x.currentTarget.classList.add('active')
+    }
+    liTags[i].onmouseleave = function (x) {
+        x.currentTarget.classList.remove('active')
+
+    }
 }
